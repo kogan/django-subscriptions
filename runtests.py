118effe0c4ca64305df9ed3596b1b947167233d0
Josh Smeaton <josh.smeaton@gmail.com>
2019-10-04 21:10:19 +1000

    Display test output in Circle

--- runtests.py
+++ runtests.py
@@ -23,6 +23,9 @@ SETTINGS_DICT = {
         "subscriptions.apps.SubscriptionsConfig",
     ],
     "DATABASES": {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}},
+    "TEST_RUNNER": "xmlrunner.extra.djangotestrunner.XMLTestRunner",
+    "TEST_OUTPUT_VERBOSE": 2,
+    "TEST_OUTPUT_DIR": "test-results",
 }
 
 
